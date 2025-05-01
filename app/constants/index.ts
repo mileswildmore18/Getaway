705ab558f85e2db027d00a771f3421540d3cfc99
Miles Wildmore <mileswildmore18@gmail.com>
2025-05-01 16:43:03 -0400

    Added the custom colours for the webpage to fit the theme. Also added the index file that will show the information about the houses and landmarks in the constants file with the index with information about each property.

---
+++ app/constants/index.ts
@@ -0,0 +1,48 @@
+// House data
+export const addresses = [
+    {
+        id: 1,
+        name: "Gulf GetAway",
+        imageUrls: [''],
+        itinerary: [{location:""}],
+        tags: [''],
+        description: "",
+        estimatedPrice: "$1000"
+    },
+    {
+        id: 2,
+        name: "Gulf GetAway",
+        imageUrls: [''],
+        itinerary: [{location:""}],
+        tags: [''],
+        description: "",
+        estimatedPrice: "$900"
+    },
+    {
+        id: 3,
+        name: "Gulf GetAway",
+        imageUrls: [''],
+        itinerary: [{location:""}],
+        tags: [''],
+        description: "",
+        estimatedPrice: "$8000"
+    },
+    {
+        id: 4,
+        name: "Gulf GetAway",
+        imageUrls: [''],
+        itinerary: [{location:""}],
+        tags: [''],
+        description: "",
+        estimatedPrice: "$1000"
+    },
+    {
+        id: 5,
+        name: "Gulf GetAway",
+        imageUrls: [''],
+        itinerary: [{location:""}],
+        tags: [''],
+        description: "",
+        estimatedPrice: "$1000"
+    }
+]
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div>Home</div>
+        <h1 className="text-5xl font-bold flex justify-center bg-yellow-200 text-green-800 p-6">Gulf GetAway</h1>
     )
 }
 export default Home
