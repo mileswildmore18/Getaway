e6ad283e93ff446808a9b0e3a66836a87d6bc85b
Miles Wildmore <mileswildmore18@gmail.com>
2025-05-15 11:30:51 -0400

    Altered the pathway for dreamhouse location.

--- app/routes.ts
+++ app/routes.ts
@@ -3,5 +3,4 @@ import {type RouteConfig, index, route} from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("Dreamhouse", "routes/dreamhouse.tsx"),
-    //route("DreamhouseId", "routes/id/DreamHouseId.$id.tsx")
 ] satisfies RouteConfig;
