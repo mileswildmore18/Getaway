fc3df6c5f4a801c22280ff54bb0cf04b851a4806
Miles Wildmore <mileswildmore18@gmail.com>
2025-06-03 11:51:46 -0400

    Added more short term rentals to the categories for sale along with giving information to call for prices.

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -3,9 +3,9 @@ export const addresses = [
     {
         id: 1,
         name: "Clearwater Beach",
-        imageUrls: ['Clearwater-Beach.jpg', 'Clearwater-home.jpg', 'Clearwater-home2.jpg', 'Clearwater-home3.jpg', 'Clearwater-home5.jpg' ,'Clearwater-home6.jpg', 'Clearwater-home7.jpg', 'Clearwater-home8.jpg', 'Clearwater-home4.jpg'],
-        itinerary: [{location:"Clearwater"}],
-        tags: [' Ocean view |', ' Wifi |', ' Free Parking |', ' Sea view |', ' TV |', ' Free washer |', ' Shared Beach access |', ' Kitchen |' , ' Dedicated workspace '],
+        imageUrls: ['Clearwater-Beach.jpg', 'Clearwater-home.jpg', 'Clearwater-home2.jpg', 'Clearwater-home3.jpg', 'Clearwater-home5.jpg', 'Clearwater-home6.jpg', 'Clearwater-home7.jpg', 'Clearwater-home8.jpg', 'Clearwater-home4.jpg'],
+        itinerary: [{location: "Clearwater"}],
+        tags: [' Ocean view |', ' Wifi |', ' Free Parking |', ' Sea view |', ' TV |', ' Free washer |', ' Shared Beach access |', ' Kitchen |', ' Dedicated workspace '],
         address: "9 Cambria St. Clearwater, FL 33767",
         description: "5 guests . 2 bedrooms . 4 beds . 2 baths",
         estimatedPrice: ""
@@ -14,19 +14,19 @@ export const addresses = [
         id: 2,
         name: "Dunedin",
         imageUrls: ['Dunedin.jpg', 'Dunedin-LivingRoom.jpg', 'Dunedin-LivingRoom2.jpg', 'Dunedin-Kitchen.jpg', 'Dunedin-DiningRoom.jpg', 'Dunedin-Bedroom1.jpg', 'Dunedin-Bedroom2.jpg', 'Dunedin-Bedroom3.jpg', 'Dunedin-Bedroom4.jpg', 'Dunedin-Bedroom5.jpg', 'Dunedin-Bathroom1.jpg', 'Dunedin-Bathroom2.jpg', 'Dunedin-Bathroom3.jpg', 'Dunedin-Bathroom4.jpg', 'Dunedin-Bathroom5.jpg', 'Dunedin-Bathroom6.jpg', 'Dunedin-Laundryroom.jpg', 'Dunedin-Backyard.jpg'],
-        itinerary: [{location:"Dunedin"}],
+        itinerary: [{location: "Dunedin"}],
         tags: ['Lake access |', ' Wifi |', ' Washer |', ' TV |', ' Kitchen |', ' Dedicated Workspace |', ' Free Parking |', ' Air conditioning |', ' Free Dryer |'],
-        address: "Pinewood Dr. Dunedin, FL 34698",
+        address: "1089 Virginia St. Dunedin, FL 34698",
         description: "12 guests . 5 bedrooms . 8 beds . 5.5 baths",
         estimatedPrice: ""
     },
     {
         id: 3,
         name: "Tarpon Springs",
-        imageUrls: ['Tarpon-Springs.jpg', 'Tarpon-Springs-LivingRoom.jpg', 'Tarpon-Springs-DiningRoom.jpg', 'Tarpon-Springs-Kitchen.jpg', 'Tarpon-Springs-Gameroom.jpg', 'Tarpon-Springs-UtilityRoom.jpg', 'Tarpon-Springs-Bedroom1.jpg', 'Tarpon-Springs-Bedroom2.jpg', 'Tarpon-Springs-Bedroom3.jpg', 'Tarpon-Springs-Bathroom1.jpg','Tarpon-Springs-Bathroom2.jpg', 'Tarpon-Springs-Outdoor1.jpg', 'Tarpon-Springs-Outdoor2.jpg'],
-        itinerary: [{location:"Tarpon Springs"}],
+        imageUrls: ['Tarpon-Springs.jpg', 'Tarpon-Springs-LivingRoom.jpg', 'Tarpon-Springs-DiningRoom.jpg', 'Tarpon-Springs-Kitchen.jpg', 'Tarpon-Springs-Gameroom.jpg', 'Tarpon-Springs-UtilityRoom.jpg', 'Tarpon-Springs-Bedroom1.jpg', 'Tarpon-Springs-Bedroom2.jpg', 'Tarpon-Springs-Bedroom3.jpg', 'Tarpon-Springs-Bathroom1.jpg', 'Tarpon-Springs-Bathroom2.jpg', 'Tarpon-Springs-Outdoor1.jpg', 'Tarpon-Springs-Outdoor2.jpg'],
+        itinerary: [{location: "Tarpon Springs"}],
         tags: [' Shared Beach access |', ' Fast Wifi - 335 Mbps |', ' Free Parking |', ' 65 inch HDTV with streaming services |', ' Exterior security cameras |', ' Kitchen |', ' Dedicated workspace |', ' Pets allowed |', ' Free washer'],
-        address: "Riverside Dr. Tarpon Springs, FL 34689",
+        address: "850 Riverside Dr. Tarpon Springs, FL 34689",
         description: "8 guests . 3 bedrooms . 5 beds . 2 baths",
         estimatedPrice: ""
     },
@@ -34,12 +34,30 @@ export const addresses = [
         id: 4,
         name: "Waynesville",
         imageUrls: ['Waynesville.jpg', 'Waynesville-Backyard.jpg', 'Waynesville-Living-Room.jpg', 'Waynesville-Kitchen.jpg', 'Waynesville-Dining.jpg', 'Waynesville-Bedroom1.jpg', 'Waynesville-Bedroom2.jpg', 'Waynesville-Bedroom3.jpg', 'Waynesville-Bedroom4.jpg', 'Waynesville-Bathroom1.jpg', 'Waynesville-Bathroom2.jpg', 'Waynesville-Bathroom3.jpg', 'Waynesville-Gameroom.jpg', 'Waynesville-Exterior.jpg', 'Waynesville-Hottub.jpg'],
-        itinerary: [{location:"North Carolina"}],
+        itinerary: [{location: "North Carolina"}],
         tags: [' Waterfront |', ' Wifi |', ' Free Parking |', ' Pets allowed |', ' Washer |', ' Kitchen |', ' Dedicated workspace |', ' Hot tub |', 'HDTV with Roku and streaming services |', ' Exterior security cameras'],
-        address: "Brushy Creek Ln. Waynesville, NC 28786",
+        address: "222 Brushy Creek Ln. Waynesville, NC 28785",
         description: "8 guests . 4 bedrooms . 5 beds . 3 baths",
         estimatedPrice: ""
     },
+    {
+        id: 5,
+        name: "Dunedin - Park Street",
+        imageUrls: ['Dunedin-Rental-Park-St.jpg', 'Dunedin-Rental-Park-St-Living-Room.jpg', 'Dunedin-Rental-Park-St-Bedroom.jpg', 'Dunedin-Rental-Park-St-Bedroom-view2.jpg', 'Dunedin-Rental-Park-St-Bathroom.jpg', 'Dunedin-Rental-Park-St-Kitchen.jpg', 'Dunedin-Rental-Park-St-Kitchen-view2.jpg'],
+        itinerary: [{location: "Dunedin"}],
+        tags: [' Courtyard view |', ' Wifi |', ' Free parking on premises |', ' Free washer |', ' AC |', ' Kitchen |', ' Dedicated workspace |', ' TV |', ' Free dryer |'],
+        address: "541 Park St. Dunedin, FL 34698",
+        description: "2 guests . 1 bedroom . 1 beds . 1 bath"
+    },
+    {
+        id: 6,
+        name: "Dunedin - Louden Avenue",
+        imageUrls: ['Dunedin-Louden-Ave.jpg', 'Dunedin-Louden-Ave-Entrance.jpg', 'Dunedin-Louden-Ave-Bedroom.jpg', 'Dunedin-Louden-Ave-Bedroom-view2.jpg', 'Dunedin-Louden-Ave-Bedroom2.jpg', 'Dunedin-Louden-Ave-Bedroom2-view2.jpg', 'Dunedin-Louden-Ave-LivingRoom.jpg', 'Dunedin-Louden-Ave-LivingRoom-view2.jpg', 'Dunedin-Louden-Ave-Kitchen.jpg', 'Dunedin-Louden-Ave-Kitchen-view2.jpg', 'Dunedin-Louden-Ave-Bathroom.jpg', 'Dunedin-Louden-Ave-Backyard.jpg', 'Dunedin-Louden-Ave-FrontPorch.jpg' ],
+        itinerary: [{location: "Dunedin"}],
+        tags: [' Courtyard view |', ' Wifi |', ' Kitchen |', ' Free parking on premises |', ' Washer |', ' Central air conditioning |', ' Beach access - Beachfront |', ' TV |', ' Dryer'],
+        address: "813 Louden Ave. Dunedin, FL 34698",
+        description: "6 guests . 2 bedrooms . 3 beds . 2 baths"
+    }
 ]
 
 // Location details and photos on Home page
@@ -71,5 +89,19 @@ export const locations = [{
         location: "Waynesville, North Carolina",
         tags: ['8 guests . 4 bedrooms . 5 beds . 3 baths']
     },
+    {
+        id: 5,
+        name: "Dunedin Rental - Park Street",
+        imageUrl: ['/assets/images/Dunedin-Rental-Park-St.jpg'],
+        location: "Dunedin, FL",
+        tags: ['2 guests . 1 bedroom . 1 beds . 1 bath']
+    },
+    {
+        id: 6,
+        name: "Dunedin Rental - Louden Avenue",
+        imageUrl: ['/assets/images/Dunedin-Louden-Ave.jpg'],
+        location: "Dunedin, FL",
+        tags: ['6 guests . 2 bedrooms . 3 beds . 2 baths']
+    }
 ]
 
