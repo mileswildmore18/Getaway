000569e3aa38b952ee0efc15dd33770cff8e6261
Miles Wildmore <mileswildmore18@gmail.com>
2025-06-06 13:24:38 -0400

    Added Clearwater 4th Condo with the pictures and it's amenities.

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -32,6 +32,16 @@ export const addresses = [
     },
     {
         id: 4,
+        name: "Clearwater Beach Condo 4",
+        imageUrls: ['Clearwater-Beach.jpg', 'Clearwater-Beach-Condo4-LivingRoom.jpg', 'Clearwater-Beach-Condo4-LivingRoom-view2.jpg', 'Clearwater-Beach-Condo4-Bedroom.jpg', 'Clearwater-Beach-Condo4-Bedroom-view2.jpg', 'Clearwater-Beach-Condo4-DiningRoom.jpg', 'Clearwater-Beach-Condo4-Kitchen.jpg', 'Clearwater-Beach-Condo4-Kitchen-view2.jpg', 'Clearwater-Beach-Condo4-Bedroom2.jpg', 'Clearwater-Beach-Condo4-Bedroom-view2.jpg', 'Clearwater-Beach-Condo4-Hallway.jpg', 'Clearwater-Beach-Condo4-Bathroom.jpg', 'Clearwater-Beach-Condo4-Bathroom2.jpg', 'Clearwater-Beach-Condo4-Balcony.jpg', 'Clearwater-Beach-Condo4-Balcony-view2.jpg'],
+        itinerary: [{location: "Clearwater"}],
+        tags: [' Ocean view |', ' Wifi |', ' Free Parking |', ' Sea view |', ' TV |', ' Free washer |', ' Shared Beach access |', ' Kitchen |', ' Dedicated workspace '],
+        address: "9 Cambria St. Clearwater, FL 33767",
+        description: "7 guests . 2 bedrooms . 5 beds . 2 baths",
+        estimatedPrice: ""
+    },
+    {
+        id: 5,
         name: "Virginia Street",
         imageUrls: ['Dunedin.jpg', 'Dunedin-LivingRoom.jpg', 'Dunedin-LivingRoom2.jpg', 'Dunedin-Kitchen.jpg', 'Dunedin-DiningRoom.jpg', 'Dunedin-Bedroom1.jpg', 'Dunedin-Bedroom2.jpg', 'Dunedin-Bedroom3.jpg', 'Dunedin-Bedroom4.jpg', 'Dunedin-Bedroom5.jpg', 'Dunedin-Bathroom1.jpg', 'Dunedin-Bathroom2.jpg', 'Dunedin-Bathroom3.jpg', 'Dunedin-Bathroom4.jpg', 'Dunedin-Bathroom5.jpg', 'Dunedin-Bathroom6.jpg', 'Dunedin-Laundryroom.jpg', 'Dunedin-Backyard.jpg'],
         itinerary: [{location: "Dunedin"}],
@@ -41,7 +51,7 @@ export const addresses = [
         estimatedPrice: ""
     },
     {
-        id: 5,
+        id: 6,
         name: "Park Street Studio A",
         imageUrls: ['Dunedin-Rental-Park-St.jpg', 'Dunedin-Rental-Park-St-Living-Room.jpg', 'Dunedin-Rental-Park-St-Bedroom.jpg', 'Dunedin-Rental-Park-St-Bedroom-view2.jpg', 'Dunedin-Rental-Park-St-Bathroom.jpg', 'Dunedin-Rental-Park-St-Kitchen.jpg', 'Dunedin-Rental-Park-St-Kitchen-view2.jpg'],
         itinerary: [{location: "Dunedin"}],
@@ -50,7 +60,7 @@ export const addresses = [
         description: "2 guests . 1 bedroom . 1 beds . 1 bath"
     },
     {
-        id: 6,
+        id: 7,
         name: "Park Street Studio B",
         imageUrls: ['Dunedin-Rental-Park-St-B.jpg', 'Dunedin-Rental-Park-St-B-LivingRoom.jpg', 'Dunedin-Rental-Park-St-B-LivingRoom-view2.jpg', 'Dunedin-Rental-Park-St-B-LivingRoom-view3.jpg', 'Dunedin-Rental-Park-St-B-Kitchen.jpg', 'Dunedin-Rental-Park-St-B-DiningRoom.jpg', 'Dunedin-Rental-Park-St-B-Bedroom.jpg', 'Dunedin-Rental-Park-St-B-Bathroom.jpg', 'Dunedin-Rental-Park-St-B-Bathroom-view2.jpg'],
         itinerary: [{location: "Dunedin"}],
@@ -59,7 +69,7 @@ export const addresses = [
         description: "2 guests . 1 bedroom . 1 beds . 1 bath"
     },
     {
-        id: 7,
+        id: 8,
         name: "Louden Avenue",
         imageUrls: ['Dunedin-Louden-Ave.jpg', 'Dunedin-Louden-Ave-Bedroom.jpg', 'Dunedin-Louden-Ave-Bedroom-view2.jpg', 'Dunedin-Louden-Ave-Bedroom2.jpg', 'Dunedin-Louden-Ave-Bedroom2-view2.jpg', 'Dunedin-Louden-Ave-LivingRoom.jpg', 'Dunedin-Louden-Ave-LivingRoom-view2.jpg', 'Dunedin-Louden-Ave-Kitchen.jpg', 'Dunedin-Louden-Ave-Kitchen-view2.jpg', 'Dunedin-Louden-Ave-Bathroom.jpg', 'Dunedin-Louden-Ave-Backyard.jpg', 'Dunedin-Louden-Ave-FrontPorch.jpg', 'Dunedin-Louden-Ave-Entrance.jpg'],
         itinerary: [{location: "Dunedin"}],
@@ -68,7 +78,7 @@ export const addresses = [
         description: "6 guests . 2 bedrooms . 3 beds . 2 baths"
     },
     {
-        id: 8,
+        id: 9,
         name: "Tarpon Springs",
         imageUrls: ['Tarpon-Springs.jpg', 'Tarpon-Springs-LivingRoom.jpg', 'Tarpon-Springs-DiningRoom.jpg', 'Tarpon-Springs-Kitchen.jpg', 'Tarpon-Springs-Gameroom.jpg', 'Tarpon-Springs-UtilityRoom.jpg', 'Tarpon-Springs-Bedroom1.jpg', 'Tarpon-Springs-Bedroom2.jpg', 'Tarpon-Springs-Bedroom3.jpg', 'Tarpon-Springs-Bathroom1.jpg', 'Tarpon-Springs-Bathroom2.jpg', 'Tarpon-Springs-Outdoor1.jpg', 'Tarpon-Springs-Outdoor2.jpg'],
         itinerary: [{location: "Tarpon Springs"}],
@@ -78,7 +88,7 @@ export const addresses = [
         estimatedPrice: ""
     },
     {
-        id: 9,
+        id: 10,
         name: "Waynesville",
         imageUrls: ['Waynesville.jpg', 'Waynesville-Backyard.jpg', 'Waynesville-Living-Room.jpg', 'Waynesville-Kitchen.jpg', 'Waynesville-Dining.jpg', 'Waynesville-Bedroom1.jpg', 'Waynesville-Bedroom2.jpg', 'Waynesville-Bedroom3.jpg', 'Waynesville-Bedroom4.jpg', 'Waynesville-Bathroom1.jpg', 'Waynesville-Bathroom2.jpg', 'Waynesville-Bathroom3.jpg', 'Waynesville-Gameroom.jpg', 'Waynesville-Exterior.jpg', 'Waynesville-Hottub.jpg'],
         itinerary: [{location: "North Carolina"}],
@@ -115,41 +125,48 @@ export const locations = [{
     },
     {
         id: 4,
+        name: "Clearwater Condo 4",
+        imageUrl: ['/assets/images/Clearwater-Beach.jpg'],
+        location: "Clearwater, FL",
+        tags: ['7 guests . 2 bedrooms . 5 beds . 2 baths']
+    },
+    {
+        id: 5,
         name: "Dunedin",
         imageUrl: ['/assets/images/Dunedin.jpg'],
         location: "Dunedin, FL",
         tags: ['12 guests . 5 bedrooms . 8 beds . 5.5 baths']
     },
     {
-        id: 5,
+        id: 6,
         name: "Dunedin Rental - Park Street A",
         imageUrl: ['/assets/images/Dunedin-Rental-Park-St.jpg'],
         location: "Dunedin, FL",
         tags: ['2 guests . 1 bedroom . 1 beds . 1 bath']
     },
     {
-        id: 6,
+        id: 7,
         name: "Dunedin Rental - Park Street B",
         imageUrl: ['/assets/images/Dunedin-Rental-Park-St-B.jpg'],
         location: "Dunedin, FL",
         tags: ['2 guests . 1 bedroom . 1 beds . 1 bath']
     },
     {
-        id: 7,
+        id: 8,
         name: "Dunedin Rental - Louden Avenue",
         imageUrl: ['/assets/images/Dunedin-Louden-Ave.jpg'],
         location: "Dunedin, FL",
         tags: ['6 guests . 2 bedrooms . 3 beds . 2 baths']
     },
     {
-        id: 8,
+        id: 9,
         name: "Tarpon Springs",
         imageUrl: ['/assets/images/Tarpon-Springs.jpg'],
         location: "Tarpon Springs, FL",
         tags: ['8 guests . 3 bedrooms . 5 beds . 2 baths']
     },
     {
-        id: 9,
+        id: 10,
         name: "Waynesville",
         imageUrl: ['/assets/images/Waynesville.jpg'],
         location: "Waynesville, North Carolina",
