09a353de2732a74d6a1e556885439a869c5cf014
Miles Wildmore <mileswildmore18@gmail.com>
2025-06-10 11:27:48 -0400

    Added the 2 remaining Clearwater Bungalows along with their respectful IDs.

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -72,6 +72,26 @@ export const addresses = [
     },
     {
         id: 8,
+        name: "Clearwater Bungalow B",
+        imageUrls: ['Clearwater-Beach-BungalowB.jpg','Clearwater-Beach-BungalowB-LivingRoom.jpg', 'Clearwater-Beach-BungalowB-LivingRoom-view2.jpg','Clearwater-Beach-BungalowB-Kitchen.jpg', 'Clearwater-Beach-BungalowB-Bedroom.jpg','Clearwater-Beach-BungalowB-Bedroom-view2.jpg', 'Clearwater-Beach-BungalowB-Bathroom.jpg', 'Clearwater-Beach-BungalowB-Bathroom-view2.jpg', 'Clearwater-Beach-BungalowB-Patio.jpg', 'Clearwater-Beach-BungalowB-Patio-view2.jpg', 'Clearwater-Beach-BungalowB-Balcony.jpg', 'Clearwater-Beach-BungalowB-Balcony-view2.jpg'],
+        itinerary: [{location: "Clearwater"}],
+        tags: [' AC |', ' Wifi |', ' Free Parking 1 space |', ' Free Dryer |', ' TV with streaming services |', ' Free washer |', ' Shared Beach access |', ' Kitchen |', ' Private patio or balcony ', ' Refrigerator'],
+        address: "9 Cambria St. Clearwater, FL 33767",
+        description: "2 guests . Studio . 1 bed . 1 bath",
+        estimatedPrice: ""
+    },
+    {
+        id: 9,
+        name: "Clearwater Bungalow C",
+        imageUrls: ['Clearwater-Beach-BungalowC.jpg','Clearwater-Beach-BungalowC-LivingRoom.jpg', 'Clearwater-Beach-BungalowC-Kitchen.jpg', 'Clearwater-Beach-BungalowC-Kitchen-view2.jpg', 'Clearwater-Beach-BungalowC-Bedroom.jpg','Clearwater-Beach-BungalowC-Bedroom2.jpg', 'Clearwater-Beach-BungalowC-Bedroom2-view2.jpg', 'Clearwater-Beach-BungalowC-Bathroom.jpg', 'Clearwater-Beach-BungalowC-Bathroom-view2.jpg', 'Clearwater-Beach-BungalowC-Patio.jpg', 'Clearwater-Beach-BungalowC-Hallway.jpg'],
+        itinerary: [{location: "Clearwater"}],
+        tags: [' Bathtub |', ' Wifi |', ' Free Parking |', ' Dryer |', ' TV |', ' Washer |', ' Beach access |', ' Kitchen |', ' Patio or balcony ', ' Air conditioning'],
+        address: "9 Cambria St. Clearwater, FL 33767",
+        description: "3 guests . 2 bedrooms . 2 beds . 1 bath",
+        estimatedPrice: ""
+    },
+    {
+        id: 10,
         name: "Virginia Street",
         imageUrls: ['Dunedin.jpg', 'Dunedin-LivingRoom.jpg', 'Dunedin-LivingRoom2.jpg', 'Dunedin-Kitchen.jpg', 'Dunedin-DiningRoom.jpg', 'Dunedin-Bedroom1.jpg', 'Dunedin-Bedroom2.jpg', 'Dunedin-Bedroom3.jpg', 'Dunedin-Bedroom4.jpg', 'Dunedin-Bedroom5.jpg', 'Dunedin-Bathroom1.jpg', 'Dunedin-Bathroom2.jpg', 'Dunedin-Bathroom3.jpg', 'Dunedin-Bathroom4.jpg', 'Dunedin-Bathroom5.jpg', 'Dunedin-Bathroom6.jpg', 'Dunedin-Laundryroom.jpg', 'Dunedin-Backyard.jpg'],
         itinerary: [{location: "Dunedin"}],
@@ -81,7 +101,7 @@ export const addresses = [
         estimatedPrice: ""
     },
     {
-        id: 9,
+        id: 11,
         name: "Park Street Studio A",
         imageUrls: ['Dunedin-Rental-Park-St.jpg', 'Dunedin-Rental-Park-St-Living-Room.jpg', 'Dunedin-Rental-Park-St-Bedroom.jpg', 'Dunedin-Rental-Park-St-Bedroom-view2.jpg', 'Dunedin-Rental-Park-St-Bathroom.jpg', 'Dunedin-Rental-Park-St-Kitchen.jpg', 'Dunedin-Rental-Park-St-Kitchen-view2.jpg'],
         itinerary: [{location: "Dunedin"}],
@@ -90,7 +110,7 @@ export const addresses = [
         description: "2 guests . 1 bedroom . 1 beds . 1 bath"
     },
     {
-        id: 10,
+        id: 12,
         name: "Park Street Studio B",
         imageUrls: ['Dunedin-Rental-Park-St-B.jpg', 'Dunedin-Rental-Park-St-B-LivingRoom.jpg', 'Dunedin-Rental-Park-St-B-LivingRoom-view2.jpg', 'Dunedin-Rental-Park-St-B-LivingRoom-view3.jpg', 'Dunedin-Rental-Park-St-B-Kitchen.jpg', 'Dunedin-Rental-Park-St-B-DiningRoom.jpg', 'Dunedin-Rental-Park-St-B-Bedroom.jpg', 'Dunedin-Rental-Park-St-B-Bathroom.jpg', 'Dunedin-Rental-Park-St-B-Bathroom-view2.jpg'],
         itinerary: [{location: "Dunedin"}],
@@ -99,7 +119,7 @@ export const addresses = [
         description: "2 guests . 1 bedroom . 1 beds . 1 bath"
     },
     {
-        id: 11,
+        id: 13,
         name: "Louden Avenue",
         imageUrls: ['Dunedin-Louden-Ave.jpg', 'Dunedin-Louden-Ave-Bedroom.jpg', 'Dunedin-Louden-Ave-Bedroom-view2.jpg', 'Dunedin-Louden-Ave-Bedroom2.jpg', 'Dunedin-Louden-Ave-Bedroom2-view2.jpg', 'Dunedin-Louden-Ave-LivingRoom.jpg', 'Dunedin-Louden-Ave-LivingRoom-view2.jpg', 'Dunedin-Louden-Ave-Kitchen.jpg', 'Dunedin-Louden-Ave-Kitchen-view2.jpg', 'Dunedin-Louden-Ave-Bathroom.jpg', 'Dunedin-Louden-Ave-Backyard.jpg', 'Dunedin-Louden-Ave-FrontPorch.jpg', 'Dunedin-Louden-Ave-Entrance.jpg'],
         itinerary: [{location: "Dunedin"}],
@@ -108,7 +128,7 @@ export const addresses = [
         description: "6 guests . 2 bedrooms . 3 beds . 2 baths"
     },
     {
-        id: 12,
+        id: 14,
         name: "Tarpon Springs",
         imageUrls: ['Tarpon-Springs.jpg', 'Tarpon-Springs-LivingRoom.jpg', 'Tarpon-Springs-DiningRoom.jpg', 'Tarpon-Springs-Kitchen.jpg', 'Tarpon-Springs-Gameroom.jpg', 'Tarpon-Springs-UtilityRoom.jpg', 'Tarpon-Springs-Bedroom1.jpg', 'Tarpon-Springs-Bedroom2.jpg', 'Tarpon-Springs-Bedroom3.jpg', 'Tarpon-Springs-Bathroom1.jpg', 'Tarpon-Springs-Bathroom2.jpg', 'Tarpon-Springs-Outdoor1.jpg', 'Tarpon-Springs-Outdoor2.jpg'],
         itinerary: [{location: "Tarpon Springs"}],
@@ -118,7 +138,7 @@ export const addresses = [
         estimatedPrice: ""
     },
     {
-        id: 13,
+        id: 15,
         name: "Waynesville",
         imageUrls: ['Waynesville.jpg', 'Waynesville-Backyard.jpg', 'Waynesville-Living-Room.jpg', 'Waynesville-Kitchen.jpg', 'Waynesville-Dining.jpg', 'Waynesville-Bedroom1.jpg', 'Waynesville-Bedroom2.jpg', 'Waynesville-Bedroom3.jpg', 'Waynesville-Bedroom4.jpg', 'Waynesville-Bathroom1.jpg', 'Waynesville-Bathroom2.jpg', 'Waynesville-Bathroom3.jpg', 'Waynesville-Gameroom.jpg', 'Waynesville-Exterior.jpg', 'Waynesville-Hottub.jpg'],
         itinerary: [{location: "North Carolina"}],
