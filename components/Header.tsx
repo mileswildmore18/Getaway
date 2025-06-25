32f08434a439b95eba98e046f63568f17577195c
Miles Wildmore <mileswildmore18@gmail.com>
2025-06-25 14:05:20 -0400

    Added an animation effect for the title for it to fade in when the user opens up the webpage.

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,13 +7,17 @@ interface Props {
 
 // Header
 const Header = ({title, description}: Props) => {
+
+
     return (
         <header className="header">
 
             {/* Title and Description of the website*/}
-            <article>
-                <h1 className={cn("text-6xl font-bold font-mono font-lugrasimo flex justify-center items-center text-center")}>{title}</h1>
-                <p className={cn("text-2xl font-bold font-mono flex justify-center bg-green-900 text-yellow-400 p-6 text-center")}>{description}</p>
+            <article className="fade-in">
+                <h1
+                    className={cn("text-6xl font-bold font-mono font-lugrasimo flex justify-center items-center text-center")}>{title}</h1>
+                <p
+                    className={cn("text-2xl font-bold font-mono flex justify-center bg-green-900 text-yellow-400 p-6 text-center")}>{description}</p>
 
                 {/* Wave Animation */}
                 <div className="wave-container">
